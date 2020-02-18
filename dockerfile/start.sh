bd5c33f8fc5a5a32e06816c49dba48d1e2775df6
pridderbusch <51443356+pridderbusch@users.noreply.github.com>
2020-02-18 14:58:42 +0100

    Update start.sh

--- dockerfile/start.sh
+++ dockerfile/start.sh
@@ -6,7 +6,7 @@ ssh-keygen -A
 # # ensure the following environment variables are set. exit script and container if not set.
 test $ssh
 
-/usr/local/bin/confd -onetime 
+/usr/local/bin/confd -onetime -backend env
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
