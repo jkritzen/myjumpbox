ee511cc2830102541506f2d9792ad8b100874b40
pridderbusch <51443356+pridderbusch@users.noreply.github.com>
2020-02-24 15:27:43 +0100

    Update start.sh

--- dockerfile/start.sh
+++ dockerfile/start.sh
@@ -1,12 +1,24 @@
+  
 #!/bin/bash
 
 # generate host keys if not present
-ssh-keygen -A
+#ssh-keygen -A
 
 # # ensure the following environment variables are set. exit script and container if not set.
-test $ssh
+#test $ssh
 
-/usr/local/bin/confd -onetime -backend env
+#/usr/local/bin/confd -onetime -backend env
+
+#Create user Folder dynamically form env-variable
+#1. Read user from env-variable and save it into variable
+value=$(</tmp/userconf.conf)
+echo "$value"
+
+#2. Create user folder from variable
+passwd -d root
+adduser $value -d /home/$value
+chown -R $value:$value /home/$value
+
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
