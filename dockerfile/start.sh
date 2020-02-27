dbb1a1ce6219599b38fc1ffbbaa670f067c22dc9
pridderbusch <51443356+pridderbusch@users.noreply.github.com>
2020-02-27 11:04:20 +0100

    Update start.sh

--- dockerfile/start.sh
+++ dockerfile/start.sh
@@ -25,8 +25,8 @@ chown -R $user:$user /home/$user
 
 #Make ssh dir
 mkdir /home/$user/.ssh
-RUN echo "$pubkey" > /home/$user/.ssh/id_rsa.pub
-RUN chmod 600 /home/$user/.ssh/id_rsa.pub
+echo "$pubkey" > /home/$user/.ssh/id_rsa.pub
+chmod 600 /home/$user/.ssh/id_rsa.pub
 
 #Create known_hosts
 touch /home/$user/.ssh/known_hosts
