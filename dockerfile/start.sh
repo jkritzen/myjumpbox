610e48ceb8bb0012c17c3a1eaf1f23ad8acc6dc7
pridderbusch <51443356+pridderbusch@users.noreply.github.com>
2020-02-27 07:50:06 +0100

    Update start.sh

--- dockerfile/start.sh
+++ dockerfile/start.sh
@@ -10,26 +10,32 @@ ssh-keygen -A
 /usr/local/bin/confd -onetime -backend env
 
 #Create user Folder dynamically form env-variable and set pub-key
-#1. Read user from env-variable and save it into variable
+#Read user from env-variable and save it into variable
 user=$(</tmp/userconf.conf)
 echo "$user"
 
-#1.1 Get pub-key
+#Get pub-key
 pubkey=$(</tmp/pubkeyconf.conf)
 echo "$pubkey"
 
-#2. Create user folder from variable
+#Create user folder from variable
 passwd -d root
 adduser $user -d /home/$user
 chown -R $user:$user /home/$user
 
-2.2 Make ssh dir
+#Make ssh dir
 mkdir /$user/.ssh
 RUN echo "$pubkey" > /$user/.ssh/id_rsa.pub
 RUN chmod 600 /$user/.ssh/id_rsa.pub
 
-#3 Create known_hosts
+#Create known_hosts
 touch /$user/.ssh/known_hosts
+
+#Overwrite sshd
+#ssh_host_rsa_key=$(</tmp/ssh/ssh_host_rsa_key)
+#echo "$ssh_host_rsa_key"
+cp -f /tmp/ssh/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key
+cp -f $pubkey /etc/ssh/ssh_host_rsa_key.pub
 
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
