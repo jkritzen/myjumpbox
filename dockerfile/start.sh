a51f7b898dde87684faef523991ba6fc3a06359f
Joerg Kritzen <43033899+jkritzen@users.noreply.github.com>
2020-03-04 16:58:40 +0000

    Update start.sh

--- dockerfile/start.sh
+++ dockerfile/start.sh
@@ -1,4 +1,27 @@
-  
+## NEW ##
+#!/bin/bash
+# Initialize Config Files
+/usr/local/bin/confd -onetime -backend env
+
+# Delete root passwd
+passwd -d root
+
+# Create Admin User
+adduser  ${admin_user} -d /home/${admin_user}
+mkdir /home/${admin_user}/.ssh
+echo ${admin_pubkey} >  /home/${admin_user}/.ssh/authorized_keys
+chown -R ${admin_user} /home/${admin_user}
+chmod 600 /home/${admin_user}/.ssh/authorized_keys
+
+
+# generate host keys if not present
+ssh-keygen -A
+exec /usr/sbin/sshd -D -e "$@"
+
+## NEW ##
+
+ 
+ 
 #!/bin/bash
 
 # generate host keys if not present
